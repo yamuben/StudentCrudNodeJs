616b8091eb32c09331c7918d76ed0af5a0844d8a
Ben <yamubbenjamin@gmail.com>
2022-07-02 11:54:46 +0200

    updated controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,28 +1,29 @@
 import UserServices from "../services/userService";
 
 class UserController {
-    //register user contoroller
   
-    static async registerUser(req, res) {
-      const newUser = await UserServices.registerStudent(req);
+  //register user contoroller
   
-      if (!newUser) {
-        return res.status(400).json({ error: "failed to register" });
-      }
-  
-      return res.status(200).json({ message: "successfully", data: newUser });
+  static async registerUser(req, res) {
+    const newUser = await UserServices.registerStudent(req);
+
+    if (!newUser) {
+      return res.status(400).json({ error: "failed to register" });
     }
-    //get all user contoroller
-  
-    static async getAllUsers(req, res) {
-      const allStudents = await UserServices.getAllStudent();
-  
-      if (!allStudents) {
-        return res.status(400).json({ error: "failed to get all" });
-      }
-  
-      return res.status(200).json({ message: "successfully", data: allStudents });
+
+    return res.status(200).json({ message: "successfully", data: newUser });
+  }
+  //get all user contoroller
+
+  static async getAllUsers(req, res) {
+    const allStudents = await UserServices.getAllStudent();
+
+    if (!allStudents) {
+      return res.status(400).json({ error: "failed to get all" });
     }
+
+    return res.status(200).json({ message: "successfully", data: allStudents });
+  }
 }
 
 export default UserController;
